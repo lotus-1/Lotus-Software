e54b23ce7efe3943f30e86926ad742636b93ac39
Sahar <saharsaker6gmail.com>
2019-06-10 10:02:28 +0300

    fix the server

--- src/app.js
+++ src/app.js
@@ -1,43 +1,45 @@
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
-const compression = require("compression");
-const exphbs = require("express-handlebars");
+// const bodyParser = require("body-parser");
+// const compression = require("compression");
+// const exphbs = require("express-handlebars");
 
-const controllers = require("./controllers/index");
+// const controllers = require("./controllers/index");
 
 const app = express();
 
-app.set("view engine", "hbs");
+// app.set("view engine", "hbs");
+//
+// app.engine(
+//   "hbs",
+//   exphbs({
+//     extname: "hbs",
+//     layoutsDir: path.join(__dirname, "views", "layouts"),
+//     defaultLayout: "main"
+//   })
+// );
 
-app.engine(
-  "hbs",
-  exphbs({
-    extname: "hbs",
-    layoutsDir: path.join(__dirname, "views", "layouts"),
-    defaultLayout: "main"
-  })
-);
-
-
+// app.use(controllers);
 // app.disable("x-powered-by");
 
 // app.use(compression());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(
-  express.static(path.join(__dirname, "..", "public"), { maxAge: "30d" })
-);
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'signing.html'));
+});
+// app.get('/', (req, res)=>{
+//
+// })
 // app.get("/", (req, res) => {
 //   res.sendFile(__dirname, "..", "views", "signing.hbs");
 //
 // });
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "..", "views", "signing.hbs"));
-// });
 
 
-// app.use(controllers);
+
+
 module.exports = app;
