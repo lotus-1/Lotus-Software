371dbfdd42155760c5024779d54c75ea4a411e5c
sara141 <saeedsara141@gmail.com>
2019-06-16 14:54:40 +0300

    fix validate file

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -2,16 +2,14 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const { postUsers, postInfo } = require("../database/queries/postDetails");
-const getUsers = require("../database/queries/getDetails");
-
+const { getPass, getInfo } = require("../database/queries/getDetails");
 // const postInfo = require("../database/queries/postDetails");
 const validate = require("../helpers/validate");
 const { loginValidation, signupValidation } = require("../helpers/validation");
 const hashPsw = require("../helpers/hashing");
 const createCookie = require("../helpers/createJwt");
-
+const conditions = require("../helpers/details");
 // router.get("/userdeemail=mahaforo276%40gmail.com&psw=511tails", getDetails);
-
 // router.post("/userdetails", postDetails);
 
 router.get("/", (req, res) => {
@@ -37,42 +35,59 @@ router.post("/register", validate(signupValidation), (req, res) => {
           // })
         });
       }
-
-
     });
   }
+});
+
 
 router.get("/login", (req, res) => {
   res.render(path.join(__dirname, "..", "views", "login"));
 });
 
-
 router.post("/login", validate(loginValidation), (req, res) => {
   const { email, password } = req.body;
-  getUsers(email, password)
-  console.log('this is password login: ', password);
-  createCookie({ email, password }, (err, result) => {
-    if (err) console.log(err);
-    else {
-      console.log(result);
-      res.cookie("jwt", result);
-      res.redirect('/home');
+
+  getPass(email, (error, hashedPassword) => {
+    console.log('this is the hashedPassword:', hashedPassword);
+    if (error) res.send("Username or password is not correct !");
+    if (!hashedPassword) {
+      res.send("<h3> No user found !</h3>");
+    } else {
+      compare(password, hashedPassword, (err, passMatch) => {
+        if (err) console.log(err);
+        if (!passMatch) {
+          res.send("<h3> Password don't match ! </h3>");
+        } else {
+          createCookie({ email, password }, (e, result) => {
+            if (e) console.log(e);
+            else {
+              console.log(result);
+              res.cookie("jwt", result);
+            }
+          });
+        }
+      });
     }
   });
+  // res.redirect("/home");
+
+  res.render(path.join(__dirname, "..", "views", "home"));
+
 });
 
 router.get("/details", (req, res) => {
+
   res.render(path.join(__dirname, "..", "views", "details"));
 });
 
 // router.post("/details", (req, res) => {
 // }
 
-router.get("/userdetails", (req, res) => {
-  getUsers.getUsers((error, response) => {
-    if (error) return error;
-    res.json(response);
-  });
-});
+// router.get("/userdetails", (req, res) => {
+//   getPass((error, response) => {
+//     if (error) return error;
+//     res.json(response);
+//   });
+// });
 
 module.exports = router;
---
+++ src/helpers/details.js
@@ -0,0 +1,8 @@
+//
+// const conditions = (age, gender, learning, cb) => {
+//   if (age > 18 && gender === "female" && learning === "yes") {
+//   
+//   }
+// }
+//
+// module.exports = conditions;
